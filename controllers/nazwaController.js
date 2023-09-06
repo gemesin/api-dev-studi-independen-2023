94f326fd8066446960e0a537fe01f0b79879a210
unknown <nazwa-np.nazwa.np6@gmail.com>
2023-09-06 21:04:37 +0700

    tambah endpoint profile

--- controllers/nazwaController.js
+++ controllers/nazwaController.js
@@ -5,4 +5,18 @@ router.get('/', (req, res) => {
   res.send('Hello nama saya Nazwa');
 });
 
-module.exports = router;
+router.get('/pofile', (req,res) => {
+  res.send({
+    namaLengkap : 'Nazwa Nurfadhilla Phitri',
+    namaPanggilan : 'Nazwa',
+    tempatLahir : 'Bekasi',
+    tanggalLahir : '6 Desember 2003',
+    umur : '19 Tahun',
+    domisili : 'Bekasi',
+    universitas : 'Universitas Negeri Padang',
+    jurusan : 'Informatika',
+    semester : '5'
+  })
+})
+
+module.exports = router; 
