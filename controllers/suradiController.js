16f0efd7eb8191d1573a22fac3ffb0f98f37ebdf
Meursault29 <suradi138.gmail.com>
2023-09-06 21:09:09 +0700

    menambahkan profile

--- controllers/suradiController.js
+++ controllers/suradiController.js
@@ -5,4 +5,19 @@ router.get('/', (req, res) => {
   res.send('Hello nama saya Suradi');
 });
 
+router.get('/profile', function(req,res) {
+  const datadiri = {
+    namaLengkap: 'Suradi Bayu',
+    namaPanggilan: 'suradi',
+    tempatLahir: 'Yogyakarta',
+    tanggalLahir: '28-04-2000',
+    umur: 23,
+    domisili: 'Yogya',
+    universitas: 'UNY',
+    jurusan: 'Teknik Mekatronika',
+    semester: 4
+  }
+  res.send(datadiri)
+})
+
 module.exports = router;
