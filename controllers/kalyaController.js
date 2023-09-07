be760fd24d1b6e1f3e47438242bbc05048f4932a
kalyatsabita <titakalya@gmail.com>
2023-09-07 18:26:17 +0700

    update kalyacontroller.js

--- controllers/kalyaController.js
+++ controllers/kalyaController.js
@@ -5,4 +5,18 @@ router.get('/', (req, res) => {
   res.send('Hello nama saya Kalya');
 });
 
+router.get('/profile', (req, res) => {
+  res.send({
+    namaLengkap: 'Kalya Tsabita',
+    namaPanggilan: 'Kalya',
+    tempatLahir: 'Surabaya',
+    tanggalLahir: '20 Januari 2003',
+    umur: '20 Tahun',
+    domisili: 'Surabaya',
+    universitas: 'Politeknik Elektronika Negeri Surabaya',
+    jurusan: 'D4 Teknik Telekomunikasi',
+    semester: '7',
+  })
+})
+
 module.exports = router;
