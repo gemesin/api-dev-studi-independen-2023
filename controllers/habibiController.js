06ce7ad4aaac2316fb5a0c14aac2ffb606fc1b3f
AZKI Habibi <azkalfrisi212@gmail.com>
2023-09-08 16:01:43 +0700

    edit file habibi

--- controllers/habibiController.js
+++ controllers/habibiController.js
@@ -4,15 +4,13 @@ const router = express.Router();
 router.get('/', (req, res) => {
   res.send('Hello nama saya Habibi');
 });
+
 router.get('/profile', (req, res) => {
   res.send({
     namaLengkap:"Azki Alfarisi ",
-
     namaPanggilan:" habibi",
-
     tempatLahir:"jakarta",
     tanggalLahir:"09 April 2023",
-
     umur:"20",
     domisili:"Jakarta",
     universitas:"UPN veteran Jakarta",
