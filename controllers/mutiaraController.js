d347dbea0f389e053420a5828054f25819824a4e
mutiararflnsh <rifliansahmutiara@gmail.com>
2023-09-08 12:30:47 +0700

    endpoint simple-calculator for tugas individu

--- controllers/mutiaraController.js
+++ controllers/mutiaraController.js
@@ -5,7 +5,9 @@ router.get('/', (req, res) => {
   res.send('Hello nama saya Mutiara');
 });
 
-router.get('/profile', (req, res) => {
+
+
+router.post('/profile', (req, res) => {
   res.send({
     namaLengkap: 'Mutiara Nuraisyah Dinda Rifliansah',
     namaPanggilan: 'Mutiara',
@@ -19,4 +21,33 @@ router.get('/profile', (req, res) => {
   })
 });
 
+
+router.get('/simple-calculator', (req, res) => {
+    const operasi = req.body.operasi;
+    const angka1 = req.body.angka1;
+    const angka2 = req.body.angka2;
+    let result;
+
+   switch (operasi) {
+    case 'penjumlahan':
+      result = angka1 + angka2;
+      break;
+    case 'pengurangan':
+      result = angka1 - angka2;
+      break;
+    case 'perkalian':
+      result = angka1 * angka2;
+      break;
+    case 'pembagian':
+      if (angka2 === 0) {
+        return res.status(400).json({ error: 'Pembagian dengan nol tidak diizinkan.' });
+      }
+      result = angka1 / angka2;
+      break;
+    default:
+      return res.status(400).json({ error: 'Operasi yang valid adalah penjumlahan, pengurangan, perkalian, atau pembagian.' });
+  }
+
+    res.json({result}); 
+});
 module.exports = router;
