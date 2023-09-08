ad125cbff81d6a8522a4e54b304369c0486a065d
AZKI Habibi <azkalfrisi212@gmail.com>
2023-09-08 19:36:55 +0700

    menambah endpoint simple-calculator

--- controllers/habibiController.js
+++ controllers/habibiController.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/habibi', (req, res) => {
   res.send('Hello nama saya Habibi');
 });
 
@@ -16,6 +16,35 @@ router.get('/profile', (req, res) => {
     universitas:"UPN veteran Jakarta",
     jurusan:"Teknik elektro",
     semester:"5",
+    })
   });
-});
+
+  router.post('/simple-calculator', (req, res) => {
+    const data = req.body;
+    const operasi = data.operasi;
+    const angka1 = parseFloat(data.angka1);
+    const angka2 = parseFloat(data.angka2);
+
+    let hasil;  
+    switch (operasi) {
+      case 'penjumlahan':
+        hasil = angka1 + angka2;
+        break;
+      case 'pengurangan':
+        hasil = angka1 - angka2;
+        break;
+      case 'perkalian':
+        hasil = angka1 * angka2;
+        break;
+      case 'pembagian':
+        if (angka2 === 0) {
+          return res.status(400).json({ error: 'Tidak dapat membagi dengan nol' });
+        }
+        hasil = angka1 / angka2;
+        break;
+      default:
+        return res.status(400).json({ error: 'Operasi tidak valid' });
+    }
+    res.json({hasil});
+      });
 module.exports = router;
