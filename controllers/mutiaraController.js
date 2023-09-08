c478b6ca19447ab1900156847f2281f78f16ceef
mutiararflnsh <rifliansahmutiara@gmail.com>
2023-09-08 11:44:31 +0700

    tambah endpoint profile mutiara

--- controllers/mutiaraController.js
+++ controllers/mutiaraController.js
@@ -5,4 +5,18 @@ router.get('/', (req, res) => {
   res.send('Hello nama saya Mutiara');
 });
 
+router.get('/profile', (req, res) => {
+  res.send({
+    namaLengkap: 'Mutiara Nuraisyah Dinda Rifliansah',
+    namaPanggilan: 'Mutiara',
+    tempatLahir: 'Sidoarjo',
+    tanggalLahir: '6 Agustus 2003',
+    umur: '20',
+    domisili: 'Sidoarjo',
+    universitas: 'ITS',
+    jurusan: 'Teknologi Informasi',
+    semester: '7'
+  })
+});
+
 module.exports = router;
