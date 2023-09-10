5316dd9eb9cb73d76ca962e6139cc2d02d07fcfe
[Shofhah Raihanasari] <a123@123de-MacBook-Pro.local>
2023-09-11 01:08:23 +0800

    membuat router baru data diri

--- controllers/raihanController.js
+++ controllers/raihanController.js
@@ -2,7 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Hello nama saya Raihan');
+  res.send('Hello nama saya Raihana');
 });
 
+router.get('/profile', (req, res) => {
+  res.send({
+    namaLengkap: "Shofhah Raihanasari",
+    namaPanggilan: "Raihana",
+    tempatLahir: "Banjarbaru",
+    umur: "20",
+    domisili: "Tanah Laut",
+    universitas: "Politeknik Negeri Tanah Laut",
+    jurusan: "Agroindustri",
+    semester: "5",
+
+  })})
 module.exports = router;
