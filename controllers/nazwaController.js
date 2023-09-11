9c521892af8e956c033fcee6b884fdc41391a7ab
Nazwa Nurfadhilla Phitri <112637218+nazwa-np@users.noreply.github.com>
2023-09-11 18:31:50 +0700

    Update nazwaController.js

--- controllers/nazwaController.js
+++ controllers/nazwaController.js
@@ -19,4 +19,36 @@ router.get('/profile', (req,res) => {
   })
 })
 
+router.post('/simple-calculator', (req, res) => {
+  const operasi = req.body.operasi;
+  const angka1 = req.body.angka1;
+  const angka2 = req.body.angka2;
+  let result;
+
+  switch (operasi) {
+      case 'penjumlahan':
+          result = angka1 + angka2;
+          res.send('Hasil penjumlahan dari ' + angka1 + ' + ' + angka2 + ' = ' + result);
+          break;
+      case 'pengurangan':
+          result = angka1 - angka2;
+          res.send('Hasil pengurangan dari ' + angka1 + ' - ' + angka2 + ' = ' + result);
+          break;
+      case 'perkalian':
+          result = angka1 * angka2;
+          res.send('Hasil perkalian dari ' + angka1 + ' * ' + angka2 + ' = ' + result);
+          break;
+      case 'pembagian':
+          if (angka2 === 0) {
+              return res.status(400).json({ error: 'Pembagian dengan nol tidak diizinkan.' });
+          }
+          result = angka1 / angka2;
+          res.send('Hasil pembagian dari ' + angka1 + ' / ' + angka2 + ' = ' + result);
+          break;
+      default:
+          return res.status(400).json({ error: 'Operasi yang valid adalah penjumlahan, pengurangan, perkalian, atau pembagian.' });
+  }
+});
+
+
 module.exports = router; 
