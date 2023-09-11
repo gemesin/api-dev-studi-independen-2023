c914a861573fc52d8d3bb5b053e00d8a7d2e6cc9
gilang2205 <langrhrdn@skyshi.com>
2023-09-11 16:38:21 +0700

    memperbaiki rumus

--- controllers/gilangController.js
+++ controllers/gilangController.js
@@ -19,32 +19,38 @@ router.get('/profile', (req, res) => {
   })
 })
 
-router.post('/simple-calculator', function(req, res){
-  const operasi = req.body.operasi
-  const angka1 = req.body.angka1
-  const angka2 = req.body.angka2
-  var hasil
+router.post('/simple-calculator', (req, res) => {
+  const { operasi, angka1, angka2 } = req.body;
+
+  if (typeof angka1 !== 'number' || typeof angka2 !== 'number') {
+    res.status(400).json({ error: 'angka1 dan angka2 harus berupa angka' });
+    return;
+  }
+  let hasil;
 
   switch (operasi) {
-    case "+":
-      hasil = angka1 + angka2
-      res.send(angka1 + " + " + angka2 + " = " + hasil)
+    case 'penjumlahan':
+      hasil = angka1 + angka2;
       break;
-    case "-":
-        hasil = angka1 + angka2
-        res.send(angka1 + " - " + angka2 + " = " + hasil)
-        break;
-    case "*":
-          hasil = angka1 + angka2
-          res.send(angka1 + " * " + angka2 + " = " + hasil)
-          break;
-    case "/":
-            hasil = angka1 + angka2
-            res.send(angka1 + " / " + angka2 + " = " + hasil)
-            break;          
+    case 'pengurangan':
+      hasil = angka1 - angka2;
+      break;
+    case 'perkalian':
+      hasil = angka1 * angka2;
+      break;
+    case 'pembagian':
+      if (angka2 === 0) {
+        res.status(400).json({ error: 'Tidak dapat membagi dengan nol' });
+        return;
+      }
+      hasil = angka1 / angka2;
+      break;
     default:
-      break;
-  }})
+      res.status(400).json({ error: 'Operasi tidak valid' });
+      return;
+  }
+  res.json({ hasil });
+});
 
 
 module.exports = router;
