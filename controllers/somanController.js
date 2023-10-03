9fba92f087bea07b220a69183f21d7951123242e
Abdul soman <abdul.210220008@mhs.unimal.ac.id>
2023-10-03 17:34:00 +0700

    tambah endpoint profile

--- controllers/somanController.js
+++ controllers/somanController.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Hello nama saya Soman pohan');
+  res.send('Hello nama saya Soman');
 });
 
 router.get('/profile', (req, res) => {
