07095ce5b736a2d711a033d41cfa4575bebcb42a
Abdul soman <abdul.210220008@mhs.unimal.ac.id>
2023-10-03 17:12:41 +0700

    tambah endpoint profile

--- controllers/somanController.js
+++ controllers/somanController.js
@@ -2,7 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Hello nama saya Soman');
+  res.send('Hello nama saya Soman pohan');
 });
+
+router.get('/profile', (req, res) => {
+  res.send({
+    namalengkap: 'Abdul Soman Pohan',
+    namapanggilan: 'Soman',
+    tempatlahir: 'Purba Sinomba',
+    tanggallahir: '2 Februari 2003',
+    umur: '21',
+    domisili: 'Blang pulo',
+    universitas: 'Malikussaleh',
+    jurusan: 'Ilmu Politik',
+    semester: '5',
+  })
+})
 
 module.exports = router;
